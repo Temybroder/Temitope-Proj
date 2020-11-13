873f7f2588b474a596b5d5ce77c3cd4517c1c887
Temitope Agboola <temybroder@gmail.com>
2020-11-13 02:15:14 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -120,70 +120,70 @@ router.get('/register', (req, res) => res.render('register'));
 
 // Register
 router.post('/register', (req, res) => {
-  // const { name, email, password, password2, role } = req.body;
+  const { name, email, password, password2, role } = req.body;
   console.log(req.body)
-  // let errors = [];
+  let errors = [];
 
-  // if (!name || !email || !password || !password2 || !role) {
-  //   errors.push({ msg: 'Please enter all fields' });
-  // }
+   if (!name || !email || !password || !password2 || !role) {
+     errors.push({ msg: 'Please enter all fields' });
+   }
 
-  // if (password != password2) {
-  //   errors.push({ msg: 'Passwords do not match' });
-  // }
+  if (password != password2) {
+     errors.push({ msg: 'Passwords do not match' });
+   }
 
-  // if (password.length < 6) {
-  //   errors.push({ msg: 'Password must be at least 6 characters' });
-  // }
+   if (password.length < 6) {
+     errors.push({ msg: 'Password must be at least 6 characters' });
+   }
 
-  // if (errors.length > 0) {
-  //   res.render('register', {
-  //     errors,
-  //     name,
-  //     email,
-  //     password,
-  //     password2,
-  //     role
-  //   });
-  // } else {
-  //   User.findOne({ email: email }).then(user => {
-  //     if (user) {
-  //       errors.push({ msg: 'Email already exists' });
-  //       res.render('register', {
-  //         errors,
-  //         name,
-  //         email,
-  //         password,
-  //         password2,
-  //         role
-  //       });
-  //     } else {
-  //       const newUser = new User({
-  //         name,
-  //         email,
-  //         password,
-  //         role
-  //       });
+   if (errors.length > 0) {
+     res.render('register', {
+       errors,
+       name,
+       email,
+       password,
+       password2,
+       role
+     });
+   } else {
+     User.findOne({ email: email }).then(user => {
+       if (user) {
+         errors.push({ msg: 'Email already exists' });
+         res.render('register', {
+           errors,
+           name,
+           email,
+           password,
+           password2,
+           role
+         });
+       } else {
+         const newUser = new User({
+           name,
+           email,
+           password,
+           role
+         });
 
-  //       bcrypt.genSalt(10, (err, salt) => {
-  //         bcrypt.hash(newUser.password, salt, (err, hash) => {
-  //           if (err) throw err;
-  //           newUser.password = hash;
-  //           newUser
-  //             .save()
-  //             .then(user => {
-  //               req.flash(
-  //                 'success_msg',
-  //                 'You are now registered and can log in'
-  //               );
-  //               res.redirect('/users/login');
-  //             })
-  //             .catch(err => console.log(err));
-  //         });
-  //       });
-  //     }
-  //   });
-  // }
+         bcrypt.genSalt(10, (err, salt) => {
+           bcrypt.hash(newUser.password, salt, (err, hash) => {
+             if (err) throw err;
+             newUser.password = hash;
+             newUser
+               .save()
+               .then(user => {
+                 req.flash(
+                   'success_msg',
+                   'You are now registered and can log in'
+                 );
+                 res.redirect('/users/login');
+               })
+               .catch(err => console.log(err));
+           });
+         });
+       }
+     });
+   }
 });
 
 // Login
